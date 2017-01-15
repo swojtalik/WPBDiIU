c028493a5636430934442acc34ce6b0c6b88124f
swojtalik <s.wojtalik@gmail.com>
2017-01-15 15:06:15 +0100

    Start

---
+++ SW_PF/SW_PF/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace SW_PF
+{
+    partial class frm_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pomocToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oProgramieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.btn_10 = new System.Windows.Forms.Button();
+            this.btn_11 = new System.Windows.Forms.Button();
+            this.btn_12 = new System.Windows.Forms.Button();
+            this.btn_13 = new System.Windows.Forms.Button();
+            this.btn_14 = new System.Windows.Forms.Button();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pomocToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1163, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pomocToolStripMenuItem
+            // 
+            this.pomocToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oProgramieToolStripMenuItem});
+            this.pomocToolStripMenuItem.Name = "pomocToolStripMenuItem";
+            this.pomocToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.pomocToolStripMenuItem.Text = "Pomoc";
+            // 
+            // oProgramieToolStripMenuItem
+            // 
+            this.oProgramieToolStripMenuItem.Name = "oProgramieToolStripMenuItem";
+            this.oProgramieToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.oProgramieToolStripMenuItem.Text = "O programie";
+            this.oProgramieToolStripMenuItem.Click += new System.EventHandler(this.oProgramieToolStripMenuItem_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1163, 507);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn_Exit);
+            this.panel1.Controls.Add(this.btn_14);
+            this.panel1.Controls.Add(this.btn_13);
+            this.panel1.Controls.Add(this.btn_12);
+            this.panel1.Controls.Add(this.btn_11);
+            this.panel1.Controls.Add(this.btn_10);
+            this.panel1.Controls.Add(this.btn_9);
+            this.panel1.Controls.Add(this.btn_8);
+            this.panel1.Controls.Add(this.btn_7);
+            this.panel1.Controls.Add(this.btn_6);
+            this.panel1.Controls.Add(this.btn_5);
+            this.panel1.Controls.Add(this.btn_4);
+            this.panel1.Controls.Add(this.btn_3);
+            this.panel1.Controls.Add(this.btn_2);
+            this.panel1.Controls.Add(this.btn_1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(226, 501);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_1
+            // 
+            this.btn_1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_1.Location = new System.Drawing.Point(0, 0);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(226, 23);
+            this.btn_1.TabIndex = 0;
+            this.btn_1.Text = "button1";
+            this.btn_1.UseVisualStyleBackColor = true;
+            // 
+            // btn_2
+            // 
+            this.btn_2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_2.Location = new System.Drawing.Point(0, 23);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(226, 23);
+            this.btn_2.TabIndex = 1;
+            this.btn_2.Text = "button1";
+            this.btn_2.UseVisualStyleBackColor = true;
+            // 
+            // btn_3
+            // 
+            this.btn_3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_3.Location = new System.Drawing.Point(0, 46);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(226, 23);
+            this.btn_3.TabIndex = 2;
+            this.btn_3.Text = "button1";
+            this.btn_3.UseVisualStyleBackColor = true;
+            // 
+            // btn_4
+            // 
+            this.btn_4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_4.Location = new System.Drawing.Point(0, 69);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(226, 23);
+            this.btn_4.TabIndex = 3;
+            this.btn_4.Text = "button1";
+            this.btn_4.UseVisualStyleBackColor = true;
+            // 
+            // btn_5
+            // 
+            this.btn_5.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_5.Location = new System.Drawing.Point(0, 92);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(226, 23);
+            this.btn_5.TabIndex = 4;
+            this.btn_5.Text = "button1";
+            this.btn_5.UseVisualStyleBackColor = true;
+            // 
+            // btn_6
+            // 
+            this.btn_6.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_6.Location = new System.Drawing.Point(0, 115);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(226, 23);
+            this.btn_6.TabIndex = 5;
+            this.btn_6.Text = "button1";
+            this.btn_6.UseVisualStyleBackColor = true;
+            // 
+            // btn_7
+            // 
+            this.btn_7.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_7.Location = new System.Drawing.Point(0, 138);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(226, 23);
+            this.btn_7.TabIndex = 6;
+            this.btn_7.Text = "button1";
+            this.btn_7.UseVisualStyleBackColor = true;
+            // 
+            // btn_8
+            // 
+            this.btn_8.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_8.Location = new System.Drawing.Point(0, 161);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(226, 23);
+            this.btn_8.TabIndex = 7;
+            this.btn_8.Text = "button1";
+            this.btn_8.UseVisualStyleBackColor = true;
+            // 
+            // btn_9
+            // 
+            this.btn_9.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_9.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_9.Location = new System.Drawing.Point(0, 184);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(226, 23);
+            this.btn_9.TabIndex = 8;
+            this.btn_9.Text = "button1";
+            this.btn_9.UseVisualStyleBackColor = true;
+            // 
+            // btn_10
+            // 
+            this.btn_10.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_10.Location = new System.Drawing.Point(0, 207);
+            this.btn_10.Name = "btn_10";
+            this.btn_10.Size = new System.Drawing.Size(226, 23);
+            this.btn_10.TabIndex = 9;
+            this.btn_10.Text = "button1";
+            this.btn_10.UseVisualStyleBackColor = true;
+            // 
+            // btn_11
+            // 
+            this.btn_11.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_11.Location = new System.Drawing.Point(0, 230);
+            this.btn_11.Name = "btn_11";
+            this.btn_11.Size = new System.Drawing.Size(226, 23);
+            this.btn_11.TabIndex = 10;
+            this.btn_11.Text = "button1";
+            this.btn_11.UseVisualStyleBackColor = true;
+            // 
+            // btn_12
+            // 
+            this.btn_12.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_12.Location = new System.Drawing.Point(0, 253);
+            this.btn_12.Name = "btn_12";
+            this.btn_12.Size = new System.Drawing.Size(226, 23);
+            this.btn_12.TabIndex = 11;
+            this.btn_12.Text = "button1";
+            this.btn_12.UseVisualStyleBackColor = true;
+            // 
+            // btn_13
+            // 
+            this.btn_13.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_13.Location = new System.Drawing.Point(0, 276);
+            this.btn_13.Name = "btn_13";
+            this.btn_13.Size = new System.Drawing.Size(226, 23);
+            this.btn_13.TabIndex = 12;
+            this.btn_13.Text = "button1";
+            this.btn_13.UseVisualStyleBackColor = true;
+            // 
+            // btn_14
+            // 
+            this.btn_14.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_14.Location = new System.Drawing.Point(0, 299);
+            this.btn_14.Name = "btn_14";
+            this.btn_14.Size = new System.Drawing.Size(226, 23);
+            this.btn_14.TabIndex = 13;
+            this.btn_14.Text = "button1";
+            this.btn_14.UseVisualStyleBackColor = true;
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_Exit.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btn_Exit.Location = new System.Drawing.Point(0, 478);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(226, 23);
+            this.btn_Exit.TabIndex = 14;
+            this.btn_Exit.Text = "Zamknij";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
+            // frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btn_Exit;
+            this.ClientSize = new System.Drawing.Size(1163, 531);
+            this.ControlBox = false;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frm_Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Program zaliczeniowy";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.frm_Main_KeyPress);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem pomocToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oProgramieToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Button btn_14;
+        private System.Windows.Forms.Button btn_13;
+        private System.Windows.Forms.Button btn_12;
+        private System.Windows.Forms.Button btn_11;
+        private System.Windows.Forms.Button btn_10;
+        private System.Windows.Forms.Button btn_9;
+        private System.Windows.Forms.Button btn_8;
+        private System.Windows.Forms.Button btn_7;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_1;
+    }
+}
+
---
+++ SW_PF/SW_PF/Info.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SW_PF
+{
+    public partial class frm_Info : Form
+    {
+        public frm_Info()
+        {
+            InitializeComponent();
+        }
+        public frm_Info(String InfoText) : this()
+        {
+            tb_Info.Text = InfoText;
+        }
+    }
+}
